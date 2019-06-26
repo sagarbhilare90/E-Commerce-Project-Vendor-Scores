bc48faae044f5ed6a81951817815ce883813068a
sagarbhilare90 <44532268+sagarbhilare90@users.noreply.github.com>
2019-06-26 12:50:53 +0530

    Update Scoring.R

--- Scoring.R
+++ Scoring.R
@@ -38,53 +38,99 @@ write.csv(dataset, "ScoreData.csv")
 
 
 #Grouping data based on Order Source
+library(ggplot2)
+library(dplyr)
 dataset<- read.csv("ScoreData.csv",stringsAsFactors = FALSE)
 dataset<-dataset[,3:ncol(dataset)]
 dataset$Order_count<- 1
-Order_Sum<- group_by(dataset, Order.Source)%>%summarise(OS=sum(Order_count))
-Order_Scr<- group_by(dataset, Order.Source)%>%summarise(OSc=sum(Score))
-Order_Sum$Scale<- (Order_Sum$OS-min(Order_Sum$OS))*100/(max(Order_Sum$OS)-min(Order_Sum$OS))
-Order_Sum$Scale<- Order_Sum$Scale*100/sum(Order_Sum$Scale)
-Order_Scr$Scale<- (Order_Scr$OSc-min(Order_Scr$OSc))*100/(max(Order_Scr$OSc)-min(Order_Scr$OSc))
-Order_Scr$Scale<- Order_Scr$Scale*100/sum(Order_Scr$Scale)
-
+Order_Sum<- group_by(dataset, Order.Source)%>%
+  summarise(No_of_transaction=sum(Order_count),Mean_Rating=mean(Ratings),Mean_Score=mean(Score))
 
 #Grouping data based on State
-State_Sum<- group_by(dataset, State)%>%summarise(SS=sum(Order_count))
-State_Scr<- group_by(dataset, State)%>%summarise(SSc=sum(Score))
-State_Sum$Scale<- (State_Sum$SS-min(State_Sum$SS))*100/(max(State_Sum$SS)-min(State_Sum$SS))
-State_Scr$Scale<- (State_Scr$SSc-min(State_Scr$SSc))*100/(max(State_Scr$SSc)-min(State_Scr$SSc))
-State_Sum$Scale<- State_Sum$Scale*100/sum(State_Sum$Scale)
-State_Scr$Scale<- State_Scr$Scale*100/sum(State_Scr$Scale)
+State_Sum<- group_by(dataset, State)%>%
+  summarise(No_of_transaction=sum(Order_count),Mean_Rating=mean(Ratings),Mean_Score=mean(Score))
 
 #Grouping data based on Vendor Venue
-Venue_Sum<- group_by(dataset, Vendor.Venue)%>%summarise(VS=sum(Order_count))
-Venue_Scr<- group_by(dataset, Vendor.Venue)%>%summarise(VSc=sum(Score))
-Venue_Sum$Scale<- (Venue_Sum$VS-min(Venue_Sum$VS))*100/(max(Venue_Sum$VS)-min(Venue_Sum$VS))
-Venue_Scr$Scale<- (Venue_Scr$VSc-min(Venue_Scr$VSc))*100/(max(Venue_Scr$VSc)-min(Venue_Scr$VSc))
-Venue_Sum$Scale<- Venue_Sum$Scale*100/sum(Venue_Sum$Scale)
-Venue_Scr$Scale<- Venue_Scr$Scale*100/sum(Venue_Scr$Scale)
+Venue_Sum<- group_by(dataset, Vendor.Venue)%>%
+  summarise(No_of_transaction=sum(Order_count),Mean_Rating=mean(Ratings),Mean_Score=mean(Score))
 
 #Grouping data based on Vendor Name
-Vendor_Sum<- group_by(dataset, Vendor.Name)%>%summarise(VdS=sum(Order_count))
-Vendor_Scr<- group_by(dataset, Vendor.Name)%>%summarise(VdSc=sum(Score))
-Vendor_Sum$Scale<- (Vendor_Sum$VdS-min(Vendor_Sum$VdS))*100/(max(Vendor_Sum$VdS)-min(Vendor_Sum$VdS))
-Vendor_Scr$Scale<- (Vendor_Scr$VdSc-min(Vendor_Scr$VdSc))*100/(max(Vendor_Scr$VdSc)-min(Vendor_Scr$VdSc))
-Vendor_Sum$Scale<- Vendor_Sum$Scale*100/sum(Vendor_Sum$Scale)
-Vendor_Scr$Scale<- Vendor_Scr$Scale*100/sum(Vendor_Scr$Scale)
+Vendor_Sum<- group_by(dataset, Vendor.Name)%>%
+  summarise(No_of_transaction=sum(Order_count),Mean_Rating=mean(Ratings),Mean_Score=mean(Score))
 
-#Visualisation base on Order Source
-library(ggplot2)
-Orderb<- ggplot(data = Order_Sum, aes(x=Order.Source,y= Scale,fill=Order.Source))+
-  geom_bar(stat="identity")+scale_colour_gradientn(colours=rainbow(10))+xlab("")
+#based on Order Source
+Orderb<- ggplot(data = Order_Sum, aes(x=Order.Source,y= Mean_Rating,fill=Order.Source))+
+  geom_bar(stat="identity")+scale_colour_gradientn(colours=rainbow(10))+ggtitle("Average ratings Vs Source of Order") +
+xlab("Source of Order")+
+ylab("Average Rating")
 
-Orderpie<- ggplot(data=Order_Sum, aes(x = "", y = Scale, fill = Order.Source))+geom_bar(width = 1, stat = "identity", color = "white") +
-  coord_polar("y", start = 0)+scale_colour_gradientn(colours=rainbow(2))
+Orderb1<- ggplot(data = Order_Sum, aes(x=Order.Source,y= Mean_Score,fill=Order.Source))+
+  geom_bar(stat="identity")+scale_colour_gradientn(colours=rainbow(10))+ggtitle("Average Score Vs Source of Order") +
+  xlab("Source of Order")+
+  ylab("Average Score")
 
-#Visualisation base on State
-library(ggplot2)
-Stateb<- ggplot(data = State_Sum, aes(x=State,y= Scale,fill=State))+
-  geom_bar(stat="identity")+scale_colour_gradientn(colours=rainbow(10))
 
-Statepie<- ggplot(data=State_Sum, aes(x = "", y = Scale, fill = State))+geom_bar(width = 1, stat = "identity", color = "white") +
-  coord_polar("y", start = 0)+scale_colour_gradientn(colours=rainbow(2))
+#Visualisation based on State
+Stateb<- ggplot(data = State_Sum, aes(x=State,y= Mean_Rating,fill=State))+
+  geom_bar(stat="identity")+scale_colour_gradientn(colours=rainbow(10))+
+  ggtitle("Average ratings Vs State") +
+  xlab("State")+
+  ylab("Average Rating")
+
+Stateb1<- ggplot(data = State_Sum, aes(x=State,y= Mean_Score,fill=State))+
+  geom_bar(stat="identity")+scale_colour_gradientn(colours=rainbow(10))+ggtitle("Average Score Vs State") +
+  xlab("State")+
+  ylab("Average Score")
+
+#Visualisation based on Vendor Venue
+Venueb<- ggplot(data = Venue_Sum, aes(x=Vendor.Venue,y= Mean_Rating,fill=Vendor.Venue))+
+  geom_bar(stat="identity")+scale_colour_gradientn(colours=rainbow(10))+
+  ggtitle("Average ratings Vs Vendor Venue") +
+  xlab("Vendor Venue")+
+  ylab("Average Rating")
+
+Venueb1<- ggplot(data = Venue_Sum, aes(x=Vendor.Venue,y= Mean_Score,fill=Vendor.Venue))+
+  geom_bar(stat="identity")+scale_colour_gradientn(colours=rainbow(10))+ggtitle("Average Score Vs Vendor Venue") +
+  xlab("Vendor venue")+
+  ylab("Average Score")
+
+#Visualisation based on Vendor Name
+Vendorb<- ggplot(data = Vendor_Sum, aes(x=Vendor.Name,y= Mean_Rating,fill=Vendor.Name))+
+  geom_bar(stat="identity")+scale_colour_gradientn(colours=rainbow(10))+
+  ggtitle("Average ratings Vs Vendor name") +
+  xlab("Vendor Name")+
+  ylab("Average Rating")
+
+Vendorb1<- ggplot(data = Vendor_Sum, aes(x=Vendor.Name,y= Mean_Score,fill=Vendor.Name))+
+  geom_bar(stat="identity")+scale_colour_gradientn(colours=rainbow(10))+ggtitle("Average Score Vs Vendor Name") +
+  xlab("Vendor Name")+
+  ylab("Average Score")
+
+VisualSelect<- function(num){
+  
+  if (num==1){Orderb
+    Orderb1}
+  else if (num==2){
+    Stateb
+    Stateb1
+  }
+  else if (num==3){
+    Venueb
+    Venueb1
+  }
+  else if(num==4){
+    Vendorb
+    Vendorb1
+  }
+  else {
+    print("Wrong Input")
+  }
+}
+
+print("Dependence on Order Source  : 1
+Dependence on State         : 2
+Dependence on vendor Venue  : 3
+Dependence on vendor name   : 4")
+num= readline("Enter the Visual Number")
+num=as.numeric(num)
+VisualSelect(num)
